417ad77abc6816b4411ca00cea9b543cc836bdeb
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 15:50:44 +0300

    working on my appointment controller show route

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,5 +2,16 @@ class AppointmentsController < ApplicationController
     def index 
         render json: Appointment.all 
     end
-    
+   
+    def show
+        appointment = find_appointment
+        render json: appointment
+    end
+
+    private
+
+    def find_appointment
+        Appointment.find(params[:id])
+    end
+
 end
