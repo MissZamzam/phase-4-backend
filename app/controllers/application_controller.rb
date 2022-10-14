f4276dede42168c6d0309b3b7f8e5b8f26064ec6
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 16:21:26 +0300

    finishing on my rails API application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ rescue_from ActiveRecord::RecordInvalid, with: :not_processable_entity
       @current_user = User.find_by(id: session[:user_id])
       render json: {errors: ["Not Authorized"]}, status: :unauthorized unless @current_user
     end
-    
+
     def not_processable_entity(exception)
       render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
     end
