9a96584609b9abdd0d879ee2ca3b81a4b214b673
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 16:09:41 +0300

    working on the create route for the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,14 @@ class UsersController < ApplicationController
         render json: @users
     end
 
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
 end
