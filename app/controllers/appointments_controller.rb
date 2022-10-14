9d55e8852697bf3999072a9d90db9a1b39b80d99
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 15:53:01 +0300

    working on my appointment controller create router

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,10 +8,20 @@ class AppointmentsController < ApplicationController
         render json: appointment
     end
 
+
+    def create 
+        appointment = Appointment.create(appointment_param)
+        render json: appointment
+    end
+
     private
 
     def find_appointment
         Appointment.find(params[:id])
+    end
+
+    def appointment_param
+        params.permit(:customer_id, :service_id, :name, :email, :appointment_date, :appointment_time)
     end
 
 end
