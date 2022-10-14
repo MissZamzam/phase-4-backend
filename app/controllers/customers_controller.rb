5307712bf0425cbb6818fe9ba61587be282ab6d3
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 15:58:26 +0300

    working on the service controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,16 @@
 class CustomersController < ApplicationController
+    def index 
+        render json: Customer.all
+    end
+
+    def show 
+        customer = find_customer
+        render json: customer
+    end
+
+    private
+    
+    def find_customer
+        Customer.find(params[:id])
+    end
 end
